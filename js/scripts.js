56c126cec7ad7c12f7ba3bef89585e7ea775db28
Sean Slingerland <seanasdfgh@gmail.com>
2017-08-25 16:24:57 -0700

    add button and hidden p to html, begin js logic

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//back end ----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//front end--------------------------------------------------
+
+$(document).ready(function() {
+  $("form#pizzaform").submit(function(event) {
+    event.preventDefault();
+
+    var userSize = $("select#size").val();
+    $.each("input[name="toppings"]:checked").val();
+
+    $(".finalPrice").append
+  })
+})
