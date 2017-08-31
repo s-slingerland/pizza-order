6953958e6679dba11426cb36a5aa64b7c949dca6
Sean Slingerland <seanasdfgh@gmail.com>
2017-08-30 22:10:44 -0700

    increased functionality in js, though still buggy

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 //back end ----------------------------------------------
 
+function Pizza(size, topping,) {
+  this.size = size;
+  this.topping = topping;
+}
 
+Pizza.prototype.finalPrice = function() {
+  var total = 0
+  if (this.size === "small"){
+    total += 8;
+  } else if (this.size === "medium") {
+    total += 12;
+  } else {
+    total += 16;
+  }
 
+  for (var a = 0; a < this.topping.length; a++) {
+    total +=1;
+  }
 
-
-
-
-
-
-
-
-
-
+  return total;
+};
 
 
 //front end--------------------------------------------------
@@ -21,8 +30,14 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userSize = $("select#size").val();
-    $.each("input[name="toppings"]:checked").val();
+    var userToppings = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+    userToppings.push($(this).val());
+    });
 
-    $(".finalPrice").append
-  })
-})
+  $("#button").click(function(){
+    $(".results").show();
+    $(".finalPrice").text(userToppings.price());
+    });
+  });
+});
